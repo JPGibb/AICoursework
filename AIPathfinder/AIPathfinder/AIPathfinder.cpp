1218635efbfe975f4b2fbf0c98030611d2a8b5cf
John Gibb <40442328@live.napier.ac.uk>
2019-10-25 15:46:17 +0100

    Read the file to memory, and create the Cavern struct swith their numbers and coords. Still need to set up the conenhctions

--- AIPathfinder/AIPathfinder/AIPathfinder.cpp
+++ AIPathfinder/AIPathfinder/AIPathfinder.cpp
@@ -7,31 +7,34 @@
 #include<string>
 #include<vector>
 
+struct Cavern 
+{
+	int cav_num;
+	int xcoord;
+	int ycoord;
+	std::vector<int> connections;
+};
+
 int number_of_caverns = 0;
+std::vector<Cavern> caverns;
+std::vector<int> coords;
+//std::vector<int> matrix;
+std::vector<std::vector<int>> matrix;
 
 int main()
 {
-	std::cout << "Program started!" << std::endl; //@cleanup remove this
-
 	std::ifstream input_file("../../generated30-1/generated30-1.cav"); //@cleanup set this to open the correct file location
 
 	std::string line;
 
 	if (input_file.is_open())
 	{
-		std::cout << "File opened" << std::endl; //@cleanup remove this
-
 		getline(input_file, line);
 
-		/*
-		while (!input_file.eof())
-		{
-			getline(input_file, line);
-		}
-		*/
-
-		std::vector<int> caves;
+		std::vector<int> cleaned_input;
 		std::string s;
+
+		//separate all the values from the file by comma
 		for (int i = 0; i < line.size(); ++i)
 		{
 			if (line[i] != ',')
@@ -40,14 +43,45 @@ int main()
 			}
 			else
 			{
-				caves.push_back(std::stoi(s));
+				cleaned_input.push_back(std::stoi(s));
 				s = "";	
 			}
 		}
 
-		number_of_caverns = caves[0];
-		caves.erase(caves.begin());
-	
+		number_of_caverns = cleaned_input[0];
+		cleaned_input.erase(cleaned_input.begin());
+		
+		//Loop thorugh all the coords in the caverns vector
+		for (int i = 0; i < number_of_caverns * 2; ++i)
+		{
+			coords.push_back(cleaned_input[i]);
+		}
+
+		//Put all the matrix values into the matrix vector
+		//This creates a table similar to the one in coursework spec
+		//Each vector within the matrix vector is a row in the table
+		std::vector<int> v;
+		for (int i = number_of_caverns * 2; i < cleaned_input.size(); ++i)
+		{
+			v.push_back(cleaned_input[i]);
+			if (v.size() == 30)
+			{
+				matrix.push_back(v);
+				v.clear(); 
+			}
+		}
+
+		//Set up the Cavern structs
+		int next_cav_num = 1;
+		for (int i = 0; i < number_of_caverns * 2; i+=2)
+		{
+			struct Cavern c;
+			c.cav_num = next_cav_num;
+			c.xcoord = coords[i];
+			c.ycoord = coords[i + 1];
+			++next_cav_num;
+			caverns.push_back(c);
+		}
 	}
 	else
 	{
