a132012c7bcb39d751647a040bd2f2736f5fb223
John Gibb <40442328@live.napier.ac.uk>
2019-10-25 16:21:16 +0100

    Fixed matrix

--- AIPathfinder/AIPathfinder/AIPathfinder.cpp
+++ AIPathfinder/AIPathfinder/AIPathfinder.cpp
@@ -59,19 +59,34 @@ int main()
 
 		//Put all the matrix values into the matrix vector
 		//This creates a table similar to the one in coursework spec
-		//Each vector within the matrix vector is a row in the table
+		//Each vector within the table vector is a row in the table
 		std::vector<int> v;
+		std::vector<std::vector<int>> table;
 		for (int i = number_of_caverns * 2; i < cleaned_input.size(); ++i)
 		{
 			v.push_back(cleaned_input[i]);
 			if (v.size() == 30)
 			{
-				matrix.push_back(v);
+				table.push_back(v);
 				v.clear(); 
 			}
 		}
-
-		//Set up the Cavern structs
+		
+		//set matrix vector to be conections for each to the caverns
+		std::vector<int> temp;
+		for (int i = 0; i < table.size(); ++i)
+		{
+			for (int j = 0; j < number_of_caverns - 1; ++j)
+			{
+				temp.push_back(table[j][i]);
+				std::cout << table[j][i];
+			}
+			matrix.push_back(temp);
+			temp.clear();
+			std::cout << std::endl;
+		}
+		
+		//Set up the Cavern structs with numbers and coords
 		int next_cav_num = 1;
 		for (int i = 0; i < number_of_caverns * 2; i+=2)
 		{
@@ -81,6 +96,12 @@ int main()
 			c.ycoord = coords[i + 1];
 			++next_cav_num;
 			caverns.push_back(c);
+		}
+
+		//add the connections to the caverns
+		for (int i = 0; i < caverns.size(); ++i)
+		{
+			
 		}
 	}
 	else
